9c15674b23a50a5589c707c20b6822297ead3a01
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-24 11:03:40 +0600

    added navbar

--- newspaper-client/src/LayOut/LayOut.jsx
+++ newspaper-client/src/LayOut/LayOut.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import StickyFooter from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const LayOut = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
       <StickyFooter></StickyFooter>
     </div>
