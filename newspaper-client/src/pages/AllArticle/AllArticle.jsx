4a5ff5114e340286bf911df84659598a2aceaa8d
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-25 20:42:01 +0600

    added all article page

--- newspaper-client/src/pages/AllArticle/AllArticle.jsx
+++ newspaper-client/src/pages/AllArticle/AllArticle.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
+import { Helmet } from "react-helmet-async";
 
 const AllArticle = () => {
   const axiosSecure = useAxiosSecure();
-  const { data: article } = useQuery({
+  const { data: articles = [] } = useQuery({
     queryKey: ["article "],
     queryFn: async () => {
       const res = await axiosSecure.get("/article", {
@@ -14,10 +16,61 @@ const AllArticle = () => {
       return res.data;
     },
   });
+  console.log(articles);
 
   return (
     <div>
-      <h2>All articles</h2>
+      <Helmet>
+        <title>Newspaper || All Article</title>
+      </Helmet>
+      <h2>All articles{articles.length}</h2>
+      <div className="grid grid-cols-3">
+        {articles.map((article) => (
+          <div
+            key={article._id}
+            className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+          >
+            <a href="#">
+              <img className="rounded-t-lg" src={article.image} alt="" />
+            </a>
+            <div className="p-5">
+              <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {article.title}
+                </h5>
+              </a>
+
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {article.publisher}
+              </p>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                {article.description}
+              </p>
+              <Link
+                to={article._id}
+                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Details
+                <svg
+                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
