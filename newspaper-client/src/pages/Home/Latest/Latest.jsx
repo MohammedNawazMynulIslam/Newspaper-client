6bbdb8eb20fbbf5ec2ba1df687c7e6d0466c640a
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-11-25 13:11:41 +0600

    fix the navbar

---
+++ newspaper-client/src/pages/Home/Latest/Latest.jsx
@@ -0,0 +1,9 @@
+const Latest = () => {
+  return (
+    <div>
+      <h2>latest</h2>
+    </div>
+  );
+};
+
+export default Latest;
--- newspaper-client/src/pages/Shared/Navbar/Navbar.jsx
+++ newspaper-client/src/pages/Shared/Navbar/Navbar.jsx
@@ -14,6 +14,223 @@ import MenuItem from "@mui/material/MenuItem";
 import { Link as RouterLink } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 
+// const Navbar = () => {
+//   const { user, logOut } = useAuth();
+//   const [anchorElNav, setAnchorElNav] = React.useState(null);
+//   const [anchorElUser, setAnchorElUser] = React.useState(null);
+//   const settings = ["Profile"];
+
+//   const handleLogOut = () => {
+//     logOut()
+//       .then(() => {})
+//       .catch((err) => console.log(err));
+//   };
+
+//   const handleOpenNavMenu = (event) => {
+//     setAnchorElNav(event.currentTarget);
+//   };
+
+//   const handleOpenUserMenu = (event) => {
+//     setAnchorElUser(event.currentTarget);
+//   };
+
+//   const handleCloseNavMenu = () => {
+//     setAnchorElNav(null);
+//   };
+
+//   const handleCloseUserMenu = () => {
+//     setAnchorElUser(null);
+//   };
+
+//   const pages = user
+//     ? [
+//         { label: "Home", link: "/" },
+//         { label: "Add Articles", link: "/add-articles" },
+//         { label: "All Articles", link: "/all-articles" },
+//         { label: "Subscription", link: "/subscription" },
+//         { label: "Dashboard", link: "/dashboard" },
+//         { label: "My Articles", link: "/my-articles" },
+//         { label: "Premium Articles", link: "/premium-articles" },
+//         // { label: "LogOut" },
+//       ]
+//     : [
+//         { label: "Home", link: "/" },
+//         { label: "Add Articles", link: "/add-articles" },
+//         { label: "All Articles", link: "/all-articles" },
+//         { label: "Subscription", link: "/subscription" },
+//         { label: "Dashboard", link: "/dashboard" },
+//         { label: "My Articles", link: "/my-articles" },
+//         { label: "Premium Articles", link: "/premium-articles" },
+//         { label: "Login", link: "/login" },
+//         { label: "Register", link: "/register" },
+//       ];
+
+//   return (
+//     <Box sx={{ flexGrow: 1 }}>
+//       <AppBar position="static">
+//         <Container maxWidth="xl">
+//           <Toolbar disableGutters>
+//             <img
+//               src="https://i.ibb.co/pdFcRKS/18157303-newspaper-icon-business-news.jpg"
+//               alt="logo"
+//               style={{ width: 30, marginRight: 10 }}
+//             />
+
+//             <Typography
+//               variant="h6"
+//               noWrap
+//               component="a"
+//               href="/"
+//               sx={{
+//                 mr: 2,
+//                 display: { xs: "none", md: "flex" },
+//                 fontFamily: "monospace",
+//                 fontWeight: 700,
+//                 letterSpacing: ".3rem",
+//                 color: "inherit",
+//                 textDecoration: "none",
+//               }}
+//             >
+//               Newspaper
+//             </Typography>
+//             <IconButton
+//               size="large"
+//               aria-label="account of current user"
+//               aria-controls="menu-appbar"
+//               aria-haspopup="true"
+//               onClick={handleOpenNavMenu}
+//               color="inherit"
+//             >
+//               <MenuIcon />
+//             </IconButton>
+//             <Menu
+//               id="menu-appbar"
+//               anchorEl={anchorElNav}
+//               anchorOrigin={{
+//                 vertical: "bottom",
+//                 horizontal: "left",
+//               }}
+//               keepMounted
+//               transformOrigin={{
+//                 vertical: "top",
+//                 horizontal: "left",
+//               }}
+//               open={Boolean(anchorElNav)}
+//               onClose={handleCloseNavMenu}
+//               sx={{
+//                 display: { xs: "block", md: "none" },
+//               }}
+//             >
+//               {pages.map((page) => (
+//                 <MenuItem
+//                   key={page.label}
+//                   component={RouterLink}
+//                   to={page.link}
+//                   onClick={handleCloseNavMenu}
+//                 >
+//                   <Typography textAlign="center">{page.label}</Typography>
+//                 </MenuItem>
+//               ))}
+//             </Menu>
+//             <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+//               <Typography
+//                 variant="h5"
+//                 noWrap
+//                 component="a"
+//                 href="#app-bar-with-responsive-menu"
+//                 sx={{
+//                   mr: 2,
+//                   display: { xs: "flex", md: "none" },
+//                   flexGrow: 1,
+//                   fontFamily: "monospace",
+//                   fontWeight: 700,
+//                   letterSpacing: ".3rem",
+//                   color: "inherit",
+//                   textDecoration: "none",
+//                 }}
+//               >
+//                 Newspaper
+//               </Typography>
+//             </Box>
+//             <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+//               {pages.map((page) => (
+//                 <Button
+//                   key={page.label}
+//                   component={RouterLink}
+//                   to={page.link}
+//                   onClick={handleCloseNavMenu}
+//                   sx={{ my: 2, color: "white", display: "block" }}
+//                 >
+//                   {page.label}
+//                 </Button>
+//               ))}
+//               <MenuItem onClick={handleLogOut}>
+//                 <Typography textAlign="center">Logout</Typography>
+//               </MenuItem>
+//             </Box>
+//             <Box sx={{ flexGrow: 1 }}>
+//               {user ? (
+//                 <>
+//                   <Tooltip title="Open settings">
+//                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+//                       <Avatar alt="" src={user?.photoURL} />
+//                     </IconButton>
+//                   </Tooltip>
+//                   <Menu
+//                     sx={{ mt: "45px" }}
+//                     id="menu-appbar"
+//                     anchorEl={anchorElUser}
+//                     anchorOrigin={{
+//                       vertical: "top",
+//                       horizontal: "right",
+//                     }}
+//                     keepMounted
+//                     transformOrigin={{
+//                       vertical: "top",
+//                       horizontal: "right",
+//                     }}
+//                     open={Boolean(anchorElUser)}
+//                     onClose={handleCloseUserMenu}
+//                   >
+//                     {settings.map((setting) => (
+//                       <MenuItem key={setting} onClick={handleCloseUserMenu}>
+//                         <Typography textAlign="center">{setting}</Typography>
+//                       </MenuItem>
+//                     ))}
+//                     <MenuItem onClick={handleLogOut}>
+//                       <Typography textAlign="center">Logout</Typography>
+//                     </MenuItem>
+//                   </Menu>
+//                 </>
+//               ) : (
+
+//                 <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+//                   {pages.map((page) => (
+//                     <Button
+//                       key={page.label}
+//                       component={RouterLink}
+//                       to={page.link}
+//                       onClick={handleCloseNavMenu}
+//                       sx={{ my: 2, color: "white", display: "block" }}
+//                     >
+//                       {page.label}
+//                     </Button>
+//                   ))}
+//                   <MenuItem onClick={handleLogOut}>
+//                     <Typography textAlign="center">Logout</Typography>
+//                   </MenuItem>
+//                 </Box>
+//               )}
+//             </Box>
+//           </Toolbar>
+//         </Container>
+//       </AppBar>
+//     </Box>
+//   );
+// };
+// export default Navbar;
+// ... (your imports)
+
 const Navbar = () => {
   const { user, logOut } = useAuth();
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -48,21 +265,23 @@ const Navbar = () => {
         { label: "Add Articles", link: "/add-articles" },
         { label: "All Articles", link: "/all-articles" },
         { label: "Subscription", link: "/subscription" },
-        { label: "Dashboard", link: "/dashboard" },
+        user.isAdmin && { label: "Dashboard", link: "/dashboard" },
         { label: "My Articles", link: "/my-articles" },
-        { label: "Premium Articles", link: "/premium-articles" },
-        // { label: "LogOut" },
-      ]
+        user.hasSubscription && {
+          label: "Premium Articles",
+          link: "/premium-articles",
+        },
+      ].filter(Boolean)
     : [
         { label: "Home", link: "/" },
         { label: "Add Articles", link: "/add-articles" },
         { label: "All Articles", link: "/all-articles" },
         { label: "Subscription", link: "/subscription" },
-        { label: "Dashboard", link: "/dashboard" },
-        { label: "My Articles", link: "/my-articles" },
-        { label: "Premium Articles", link: "/premium-articles" },
-        { label: "Login", link: "/login" },
-        { label: "Register", link: "/register" },
+        // { label: "Dashboard", link: "/dashboard" },
+        // { label: "My Articles", link: "/my-articles" },
+        // { label: "Premium Articles", link: "/premium-articles" },
+        // { label: "Login", link: "/login" },
+        // { label: "Register", link: "/register" },
       ];
 
   return (
@@ -93,6 +312,7 @@ const Navbar = () => {
             >
               Newspaper
             </Typography>
+
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -103,6 +323,7 @@ const Navbar = () => {
             >
               <MenuIcon />
             </IconButton>
+            {/* ... (your menu for smaller screens) */}
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
@@ -164,67 +385,141 @@ const Navbar = () => {
                   {page.label}
                 </Button>
               ))}
-              <MenuItem onClick={handleLogOut}>
-                <Typography textAlign="center">Logout</Typography>
-              </MenuItem>
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
               {user ? (
+                // <>
+                //   <MenuItem onClick={handleLogOut}>
+                //     <Typography textAlign="center">Logout</Typography>
+                //   </MenuItem>
+                //   <Tooltip title="Open settings">
+                //     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                //       <Avatar alt="" src={user?.photoURL} />
+                //     </IconButton>
+                //   </Tooltip>
+                //   <Menu
+                //     sx={{ mt: "45px" }}
+                //     id="menu-appbar"
+                //     anchorEl={anchorElUser}
+                //     anchorOrigin={{
+                //       vertical: "top",
+                //       horizontal: "right",
+                //     }}
+                //     keepMounted
+                //     transformOrigin={{
+                //       vertical: "top",
+                //       horizontal: "right",
+                //     }}
+                //     open={Boolean(anchorElUser)}
+                //     onClose={handleCloseUserMenu}
+                //   >
+                //     {settings.map((setting) => (
+                //       <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                //         <Typography textAlign="center">{setting}</Typography>
+                //       </MenuItem>
+                //     ))}
+                //     <MenuItem
+                //       component={RouterLink}
+                //       to="/profile"
+                //       onClick={handleCloseUserMenu}
+                //     >
+                //       <Typography textAlign="center">Profile</Typography>
+                //     </MenuItem>
+                //   </Menu>
+                // </>
+
                 <>
-                  <Tooltip title="Open settings">
-                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                      <Avatar alt="" src={user?.photoURL} />
-                    </IconButton>
-                  </Tooltip>
-                  <Menu
-                    sx={{ mt: "45px" }}
-                    id="menu-appbar"
-                    anchorEl={anchorElUser}
-                    anchorOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: "top",
-                      horizontal: "right",
-                    }}
-                    open={Boolean(anchorElUser)}
-                    onClose={handleCloseUserMenu}
-                  >
-                    {settings.map((setting) => (
-                      <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Typography textAlign="center">{setting}</Typography>
-                      </MenuItem>
-                    ))}
-                    <MenuItem onClick={handleLogOut}>
-                      <Typography textAlign="center">Logout</Typography>
+                  {user.isAdmin && (
+                    <MenuItem
+                      component={RouterLink}
+                      to="/dashboard"
+                      onClick={handleCloseNavMenu}
+                    >
+                      <Typography textAlign="center">Dashboard</Typography>
                     </MenuItem>
-                  </Menu>
+                  )}
+                  <MenuItem
+                    component={RouterLink}
+                    to="/my-articles"
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Typography textAlign="center">My Articles</Typography>
+                  </MenuItem>
+                  {user.hasSubscription && (
+                    <MenuItem
+                      component={RouterLink}
+                      to="/premium-articles"
+                      onClick={handleCloseNavMenu}
+                    >
+                      <Typography textAlign="center">
+                        Premium Articles
+                      </Typography>
+                    </MenuItem>
+                  )}
+                  <MenuItem
+                    component={RouterLink}
+                    to="/profile"
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Typography textAlign="center">Profile</Typography>
+                  </MenuItem>
+                  <MenuItem onClick={handleLogOut}>
+                    <Typography textAlign="center">Logout</Typography>
+                  </MenuItem>
                 </>
               ) : (
                 <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                  {pages.map((page) => (
-                    <Button
-                      key={page.label}
-                      component={RouterLink}
-                      to={page.link}
-                      onClick={handleCloseNavMenu}
-                      sx={{ my: 2, color: "white", display: "block" }}
-                    >
-                      {page.label}
-                    </Button>
-                  ))}
-                  <MenuItem onClick={handleLogOut}>
-                    <Typography textAlign="center">Logout</Typography>
+                  <MenuItem
+                    component={RouterLink}
+                    to="/login"
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Typography textAlign="center">Login</Typography>
+                  </MenuItem>
+                  <MenuItem
+                    component={RouterLink}
+                    to="/register"
+                    onClick={handleCloseNavMenu}
+                  >
+                    <Typography textAlign="center">Register</Typography>
                   </MenuItem>
                 </Box>
               )}
             </Box>
+            {user && (
+              <Box>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="" src={user?.photoURL} />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            )}
           </Toolbar>
         </Container>
       </AppBar>
     </Box>
   );
 };
+
 export default Navbar;
