69db18a2a89a610f53b1e01f4c56e10d6139dc0a
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-01-06 12:59:41 +0600

    removed the framer motion effect from all articles cause the button isnt working

--- newspaper-client/src/pages/AllArticle/AllArticle.jsx
+++ newspaper-client/src/pages/AllArticle/AllArticle.jsx
@@ -4,7 +4,7 @@ import useAxiosSecure from "../../hooks/useAxiosSecure";
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { useState } from "react";
-import { motion } from "framer-motion";
+
 const AllArticle = () => {
   const axiosSecure = useAxiosSecure();
 
@@ -139,73 +139,64 @@ const AllArticle = () => {
         ) : (
           <div className="grid justify-center lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6 mt-2">
             {articles.map((article) => (
-              <motion.div
-                key={articles.id}
-                whileHover={{ scale: 1.3 }}
-                whileTap={{ scale: 0.9 }}
-                className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+              <div
+                key={article._id}
+                className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
               >
-                <div
-                  key={article._id}
-                  className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
-                >
-                  <div className="">
-                    <img
-                      className="rounded-t-lg w-full h-[300px]"
-                      src={article.image}
-                      alt=""
-                    />
-                  </div>
-                  <div className="p-5">
-                    <a href="#">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {article.title}
-                      </h5>
-                    </a>
-
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                      {article.publisher}
-                    </p>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                      {article.tags}
-                    </p>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                      {article.description}
-                    </p>
-                    <Link to={`/article/${article._id}`}>
-                      <button
-                        className={`inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ${
-                          article.isPremium &&
-                          !isUserSubscribed(article.authorId)
-                            ? "disabled:opacity-50 cursor-not-allowed"
-                            : ""
-                        }`}
-                        disabled={
-                          article.isPremium &&
-                          !isUserSubscribed(article.authorId)
-                        }
-                      >
-                        Details
-                        <svg
-                          className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 14 10"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M1 5h12m0 0L9 1m4 4L9 9"
-                          />
-                        </svg>
-                      </button>
-                    </Link>
-                  </div>
+                <div className="">
+                  <img
+                    className="rounded-t-lg w-full h-[300px]"
+                    src={article.image}
+                    alt=""
+                  />
                 </div>
-              </motion.div>
+                <div className="p-5">
+                  <a href="#">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                      {article.title}
+                    </h5>
+                  </a>
+
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {article.publisher}
+                  </p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {article.tags}
+                  </p>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {article.description}
+                  </p>
+                  <Link to={`/article/${article._id}`}>
+                    <button
+                      className={`inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ${
+                        article.isPremium && !isUserSubscribed(article.authorId)
+                          ? "disabled:opacity-50 cursor-not-allowed"
+                          : ""
+                      }`}
+                      disabled={
+                        article.isPremium && !isUserSubscribed(article.authorId)
+                      }
+                    >
+                      Details
+                      <svg
+                        className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 14 10"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M1 5h12m0 0L9 1m4 4L9 9"
+                        />
+                      </svg>
+                    </button>
+                  </Link>
+                </div>
+              </div>
             ))}
           </div>
         )}
